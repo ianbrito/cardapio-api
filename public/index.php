307b366dc475b9b84fc92eb78aefd9751986a45f
Ian Azevedo <ianbazevedo@gmail.com>
2023-11-16 17:43:52 -0300

    adicionando suporte a .env

--- public/index.php
+++ public/index.php
@@ -2,6 +2,9 @@
 
 declare(strict_types=1);
 
-require __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/../vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__.'/..');
+$dotenv->safeLoad();
 
 require_once __DIR__ . '/../src/bootstrap.php';
